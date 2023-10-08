1cda747dda245316a6a816ed5796baafbbdfb2ed
Zac Tolle <zactolle@outlook.com>
2023-10-08 15:55:49 -0700

    #2 Started exploring basic layout

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 ![logo-title 8a56eaae](https://github.com/ZacTolle/modelprompter/assets/139601580/e302ce36-acd5-4c70-be65-04d490d30c70)
-
 # Work in progress
+
+## Motivation
+To design the easiest and most hackable Graphical User Interface (GUI) and process manager for [Microsoft Autogen](https://github.com/microsoft/autogen) and other Large Language Model (LLM) frameworks that can run from anywhere. The vision roadmap includes:
+- [ ] Setup wizard to help you configure everything (including local models)
+- [ ] Customizable no-code command palette
+- [ ] Agent code/skill viewer and editor
+- [ ] Customizable dashboards to manage multiple projects
+- [ ] No-code Autogen generator
+- [ ] Voice to text
+- [ ] Text to voice
+- [ ] Remove reliance on displays completely
+
 > This project is still in development and is not ready for use.
 
 # Local Development
@@ -15,12 +26,12 @@ git clone https://github.com/zactolle/modelprompter
 cd modelprompter
 
 # Create and activate a virtual environment
-python -m venv venv
+python -m venv env
 
 # Windows only:
-venv\Scripts\activate
+./env\Scripts\activate
 # Everywhere else:
-source venv/bin/activate
+source ./env/bin/activate
 
 # Install dependencies
 pip install -r requirements.txt
@@ -37,4 +48,4 @@ CTRL+C
 
 ## Development notes
 Ignore this I'm just very new to Python 😅:
-- `textual run --dev app.py` for CSS hot reloading
+- `textual run --dev app.py` for CSS hot reloading. `python app.py` is more for aesthetic
--- app.py
+++ app.py
@@ -1,71 +1,20 @@
-from time import monotonic
-
-from textual import on
 from textual.app import App
-from textual.reactive import reactive
-from textual.containers import ScrollableContainer
-from textual.widgets import Button, Footer, Header, Static
+from textual.widgets import Header, Placeholder
+from textual.containers import ScrollableContainer, Horizontal, Vertical
 
-class TimeDisplay(Static):
-    time_elapsed = reactive(0)
-    start_time = monotonic()
+# Create a new app
+class MP(App):
+  TITLE = "ModelPrompter"
+  CSS_PATH = "app.css"
 
-    def watch_time_elapsed(self):
-        time = self.time_elapsed
-        self.update(str(time))
+  def compose(self):
+    yield Header(show_clock=True)
+    with Horizontal():
+      with ScrollableContainer():
+        yield Placeholder("Hello, world!")
+      with ScrollableContainer():
+        yield Placeholder("Hello, world!")
 
-    def start(self):
-        self.start_time = monotonic()
-        self.time_elapsed = 0
-        pass
-
-    def stop(self):
-        self.time_elapsed = monotonic() - self.start_time
-        pass
-
-    def reset(self):
-        self.time_elapsed = 0
-        pass
-
-class Stopwatch(Static):
-    @on(Button.Pressed, '#start')
-    def start_stopwatch(self):
-        self.add_class("started")
-        self.query_one(TimeDisplay).start()
-        
-    @on(Button.Pressed, '#stop')
-    def stop_stopwatch(self):
-        self.remove_class("started")
-        self.query_one(TimeDisplay).stop()
-
-    @on(Button.Pressed, '#reset')
-    def reset_stopwatch(self):
-        self.remove_class("started")
-        self.query_one(TimeDisplay).reset()
-    
-    def compose(self):
-        yield Button('Start', variant="success", id="start")
-        yield Button('Stop', variant="error", id="stop", classes="hidden")
-        yield Button('Reset', id="reset")
-        yield TimeDisplay('00:00:00.00', id="time")
-
-class ModelPrompter(App):
-    BINDINGS = [
-        ('d', 'toggle_dark_mode', 'Toggle dark mode')
-    ]
-
-    CSS_PATH = "app.css"
-
-    def compose(self):
-        yield Header(show_clock=True)
-        with ScrollableContainer(id="stopwatches"):
-            yield Stopwatch(classes="started")
-            yield Stopwatch()
-            yield Stopwatch()
-        yield Footer()
-
-    def action_toggle_dark_mode(self):
-        self.dark = not self.dark
-
+# Run the app
 if __name__ == "__main__":
-    ModelPrompter().run()
+  MP().run()    
